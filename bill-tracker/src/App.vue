294301af7cba76f2f1df55ec2a4299df33783cc5
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-11 10:47:05 -0600

    layout the components

--- bill-tracker/src/App.vue
+++ bill-tracker/src/App.vue
@@ -1,6 +1,32 @@
 <template>
-
+  <main>
+    <NavBar />
+    <div class="container flex">
+      <div class="w-1/2">
+        <BillsTable />
+      </div>
+      <div class="w-1/2">
+        <Chart />
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
+  import AddCategory from './components/AddCategory.vue'
+  import AddBill from './components/AddBill.vue'
+  import NavBar from './components/NavBar.vue'
+  import Chart from './components/Chart.vue'
+  import BillsTable from './components/BillsTable.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      AddCategory,
+      AddBill,
+      Chart,
+      BillsTable,
+      NavBar
+    }
+  }
 </script>
