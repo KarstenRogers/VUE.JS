3fc47c5afa67318bc3511bd755440af717771094
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-11 15:25:15 -0600

    SHOWING THE LIST OF CATEGORIES, AND ADDING NEW ONES

--- bill-tracker/src/components/NavBar.vue
+++ bill-tracker/src/components/NavBar.vue
@@ -1,3 +1,23 @@
 <template>
-    <p>NavBar</p>
+    <ul class="list-reset inline flex justify-center border-b-4 mb-0">
+        <li
+                class="p-4 inline bg-grey-lighter hover:bg-grey-light uppercase font-black cursor-pointer"
+                v-for="category in categories"
+                :key="category" >
+            {{category}}
+        </li>
+
+        <li class="p-4 inline bg-grey-lighter hover:bg-grey-light uppercase font-black cursor-pointer" @click="triggerShowAddCategory">➕</li>
+    </ul>
 </template>
+
+<script>
+    export default {
+        props: ['categories'],
+        methods: {
+            triggerShowAddCategory() {
+                this.$emit('triggerShowAddCategory')
+            }
+        }
+    }
+</script>
