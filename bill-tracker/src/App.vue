39162809f6e354dbaea0c049444342438993d4d5
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-12 09:47:57 -0600

    added the delete bill

--- bill-tracker/src/App.vue
+++ bill-tracker/src/App.vue
@@ -17,6 +17,7 @@
             <BillsTable
               :bills="bills"
               v-on:triggerShowAddBill="triggerShowAddBill"
+              v-on:removeBill="removeBill"
             />
           </div>
           <div class="w-1/2">
@@ -69,6 +70,11 @@ export default {
     },
     triggerShowAddBill() {
       this.shouldShowAddBill = true;
+    },
+    removeBill(index) {
+      this.bills = this.bills
+        .slice(0, index)
+        .concat(this.bills.slice(index + 1, this.bills.length));
     }
   },
   watch: {
