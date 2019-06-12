4006fb23b1e51e41087666f1884e71840b149f67
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-12 09:01:13 -0600

    added bills

--- bill-tracker/src/App.vue
+++ bill-tracker/src/App.vue
@@ -14,7 +14,10 @@
         />
         <div class="container flex">
           <div class="w-1/2">
-            <BillsTable />
+            <BillsTable
+              :bills="bills"
+              v-on:triggerShowAddBill="triggerShowAddBill"
+            />
           </div>
           <div class="w-1/2">
             <Chart />
@@ -26,11 +29,14 @@
 </template>
 
 <script>
+import Vue from "vue";
 import AddCategory from "./components/AddCategory.vue";
 import AddBill from "./components/AddBill.vue";
 import NavBar from "./components/NavBar.vue";
 import Chart from "./components/Chart.vue";
 import BillsTable from "./components/BillsTable.vue";
+
+Vue.use(require("vue-moment"));
 
 export default {
   name: "app",
@@ -46,7 +52,7 @@ export default {
       bills: [],
       categories: [],
       shouldShowAddCategory: false,
-      shouldShowAddBill: true
+      shouldShowAddBill: false
     };
   },
   methods: {
@@ -60,6 +66,9 @@ export default {
     },
     triggerShowAddCategory() {
       this.shouldShowAddCategory = true;
+    },
+    triggerShowAddBill() {
+      this.shouldShowAddBill = true;
     }
   },
   watch: {
