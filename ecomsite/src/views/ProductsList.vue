fcd0ed286ee95237ada3ae1dd94ffe8c959f2b75
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-13 13:23:29 -0600

    list the product

--- ecomsite/src/views/ProductsList.vue
+++ ecomsite/src/views/ProductsList.vue
@@ -1,5 +1,47 @@
 <template>
-    <div class="">
-        <h1>Products List</h1>
+    <div class="products-list">
+        <div v-for="(product, index) in $store.getters.products"
+             :key="index">
+            <img :src="product.imageUrl" />
+            <h2>{{product.name}}</h2>
+            <p class="description">{{product.description}}</p>
+            <p class="price">${{product.price}}</p>
+            <button @click.stop="deleteProduct(index)">ⓧ</button>
+        </div>
     </div>
 </template>
+
+<script>
+    export default {
+        name: 'Products',
+        methods: {
+            deleteProduct(index) {
+                this.$store.commit('deleteProduct', index)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .products-list {
+        display: flex;
+        padding-top: 30px;
+    }
+    .products-list div {
+        width: 33%;
+
+        box-sizing: border-box;
+        padding: 30px;
+        background-color: lightsalmon;
+    }
+
+    button {
+        padding: 30px;
+        font-size: 2rem;
+    }
+
+    .description,
+    .price {
+        padding-top: 20px;
+    }
+</style>
