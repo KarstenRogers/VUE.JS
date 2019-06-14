da91dba52a33ca78087461586a0b4ef6be7c7668
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-14 11:48:38 -0600

    THE FORM COMPONENT

---
+++ githubusers/src/components/Form.vue
@@ -0,0 +1,37 @@
+<template>
+    <form @submit.prevent="addUser">
+        <input
+                type="text"
+                v-model="username"
+                placeholder="GitHub username"
+                required
+        />
+        <button type="submit">Add card</button>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'Form',
+        data: function() {
+            return {
+                username: ''
+            }
+        },
+        methods: {
+            addUser: function() {
+                alert(this.username)
+            }
+        }
+    }
+</script>
+
+<style>
+    button {
+        height: 40px;
+    }
+    input {
+        width: 300px;
+        height: 34px;
+    }
+</style>
