fdbc49fc8567de8f217f5251ab266c359225e0d7
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-19 09:51:02 -0600

    fixing up component

--- linkin/src/App.vue
+++ linkin/src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app" class="conatiner mt-5">
     <h1>My Shop</h1>
+    <navbar
+      :cart="cart"
+      :cartQty="cartQty"
+      :cartTotal="cartTotal"
+      @toggle="toggleSliderStatus"
+    ></navbar>
     <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum"></price-slider>
     <product-list
       :maximum="maximum"
@@ -14,6 +20,7 @@
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import ProductList from "./components/ProductList";
 import PriceSlider from "./components/PriceSlider";
+import Navbar from "./components/Navbar";
 
 export default {
   name: "app",
@@ -25,12 +32,47 @@ export default {
       products: null
     };
   },
+
   components: {
+    Navbar,
     ProductList,
     FontAwesomeIcon,
     PriceSlider
   },
+
+  computed: {
+    cartTotal: function() {
+      let sum = 0;
+      for (key in this.cart) {
+        sum = sum+(this.cart[key].product.price * this.cart[key].qty);
+      }
+      return sum;
+    },
+    cartQty: function() {
+      let qty = 0;
+      for (key in this.cart) {
+        qty = qty + this.cart[key].qty;
+      }
+      return qty;
+    },
+    sliderState: function() {
+      return this.sliderStatus ? 'd-flex': 'd-none'
+    }
+  },
+
   methods: {
+    toggleSliderStatus: function() {
+      this.sliderStatus = !this.sliderStatus;
+    },
+
+    deleteItem: function(id) {
+      if(this.cart[id].qty>1) {
+        this.cart[id].qty--;
+      } else {
+        this.cart.splice(id, 1);
+      }
+    },
+
     addItem: function(product) {
       var whichProduct;
       var existing = this.cart.filter(function(item, index) {
