c86e764b93b4e3f85eff83ceddceb397d9dfc810
Karsten Rogers <karstenrogers@Karstens-MacBook-Air.local>
2019-06-20 09:11:25 -0600

    added input and connecting input

---
+++ taskmanagement/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app" class="container">
+    <img src="./assets/logo.png" class="logo" />
+    <task-list/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import taskList from "./components/taskList";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    taskList
+  }
+};
+</script>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  .container {
+    max-width: 600px;
+    margin: 0 auto;
+  }
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+  .logo {
+    height: 75px;
+  }
+</style>
---
+++ taskmanagement/src/components/taskList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <input
+      type="text"
+      class="task-input"
+      placeholder="New Task"
+      v-model="newTask"
+      @keyup.enter="addTask"
+    />
+    <div v-for="task in tasks" :key="task.id" class="task-item">
+      {{ task.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "taskList",
+  data() {
+    return {
+      newTask: "",
+        idForTask: 3,
+      tasks: [
+        {
+          id: 1,
+          title: "Gas",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Food",
+          completed: false
+        }
+      ]
+    };
+  },
+    methods: {
+      addTask() {
+          this.tasks.push({
+              id: this.idForTask,
+              title: this.newTask,
+              completed: false,
+          })
+
+          this.newTask = ''
+          this.idForTask++
+      }
+    }
+};
+</script>
+
+<style lang="scss">
+.task-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+
+  &:focus {
+    outline: 0;
+  }
+}
+</style>
